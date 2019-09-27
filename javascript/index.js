6e3433b7e6e9ed1865a0547c68cfc6665dd5c19f
Chance Musselman <musselman.chance@icloud.com>
2019-09-26 23:04:07 -0700

    Push Changes for review, heavy workover. Design will soon most likly be reverted to previous version. Per Kelcy.

--- javascript/index.js
+++ javascript/index.js
@@ -1,14 +1,12 @@
 $(document).ready(function() {
   console.log("index.js linked");
 
-  //   let navbarItemArray = ["item 1", "item 2", "item 3", "item 4", "item 5"];
-  let itemArray2 = ["item 1", "item 2", "item 3", "item 4", "item 5"];
-  let itemArray3 = ["item 1", "item 2", "item 3", "item 4", "item 5"];
-  let itemArray4 = ["item 1", "item 2", "item 3", "item 4", "item 5"];
-  let itemArray5 = ["item 1", "item 2", "item 3", "item 4", "item 5"];
-  let itemArray6 = ["item 1", "item 2", "item 3", "item 4", "item 5"];
-  let itemArray7 = ["item 1", "item 2", "item 3", "item 4", "item 5"];
-  let itemArray8 = ["item 1", "item 2", "item 3", "item 4", "item 5"];
-  let itemArray9 = ["item 1", "item 2", "item 3", "item 4", "item 5"];
-  let itemArray10 = ["item 1", "item 2", "item 3", "item 4", "item 5"];
+  let treeHuggerImgs = function() {
+    console.log("treehuggerimg fuction ran");
+    $(".treehuggerImg").html(
+      '<img id="treehuggerImg" class="shadow" src="./images/TreeHugger1Webtindy.png"></img>'
+    );
+  };
+
+  treeHuggerImgs();
 });
